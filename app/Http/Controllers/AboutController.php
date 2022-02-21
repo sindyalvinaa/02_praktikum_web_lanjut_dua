860eb123db1b59389d310dd684737e7dc00b64f3
sindyalvinaa <sindyalvinaa93@gmail.com>
2022-02-21 23:59:24 +0700

    02_praktikum_web_lanjut_dua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "Nim : 2041720003, Nama : Sindy Alvina Khoiriyah, Kelas : TI 2D";
+    }
+}
